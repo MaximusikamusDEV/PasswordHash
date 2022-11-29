81c4e2a9ac261da62e8d54707befbd843ba5e3dc
Saness <saness729@gmail.com>
2022-11-29 21:10:56 +0300

    Entering

--- Enter.cpp
+++ Enter.cpp
@@ -9,102 +9,105 @@ using namespace std;
 
 void enter()
 {
-	const int StartSize = 1000;
-	
-	char Pass[StartSize];
-	char Login[StartSize];
-	char Comment[StartSize];
-	
+    const int StartSize = 1000;
 
-	cout << "Enter your login: ";
-	cin >> Login;
+    char Pass[StartSize];
+    char Login[StartSize];
+    char Comment[StartSize];
 
-	cout << "Enter your password: ";
-	cin >> Pass;
+    do
+    {
+        cout << "Enter your login: ";
+        cin >> Login;
 
-	int size;
+        cout << "Enter your password: ";
+        cin >> Pass;
 
-	size = strlen(Pass);
+        int size;
 
-	if (size < 8)
-	{
-		cout << "Your's password size is too low. Try again";
-		return;
-	}
+        size = strlen(Pass);
 
-		bool fllett = 0, flnum = 0, flspec = 0;
-		bool fl1, fl2;
+        if (size < 8)
+        {
+            cout << "Your's password size is too low. Try again\n";
+            continue;
+        }
 
-	for (int i = 0; i < size; i++)
-	{
-		fl1 = 0;
-		fl2 = 0;
+        bool fllett = 0, flnum = 0, flspec = 0;
+        bool fl1, fl2;
 
-		if (Pass[i] >= 'a' && Pass[i] <= 'z' || Pass[i] >= 'A' && Pass[i] <= 'Z')
-		{
-			fllett = 1;
-			fl1 = 1;
-		}
-		
-		if (Pass[i] >= '0' && Pass[i] <= '9')
-		{
-			flnum = 1;
-			fl2 = 1;
-		}
+        for (int i = 0; i < size; i++)
+        {
+            fl1 = 0;
+            fl2 = 0;
 
-		if (fl1 == 0 && fl2 == 0)
-			flspec = 1;
+            if (Pass[i] >= 'a' && Pass[i] <= 'z' || Pass[i] >= 'A' && Pass[i] <= 'Z')
+            {
+                fllett = 1;
+                fl1 = 1;
+            }
 
-	}
-  
-	if (flspec == 0 || flnum == 0 || fllett == 0)
-	{
-		cout << "Your pasword is uncorrect. Please try again";
-		return;
-	}
+            if (Pass[i] >= '0' && Pass[i] <= '9')
+            {
+                flnum = 1;
+                fl2 = 1;
+            }
 
+            if (fl1 == 0 && fl2 == 0)
+                flspec = 1;
 
-	size = strlen(Login);
+        }
 
-	if (size < 8)
-	{
-		cout << "Your Login size is too low. Please trya again";
-		return ;
-	}
-
-	flspec = 0;
-	flnum = 0;
-
-	if (strchr(Login, '@') != nullptr)
-		flspec = 1;
-
-	if (strchr(Login, '.') != nullptr)
-		flnum = 1;
-
-	if (flnum == 0 || flspec == 0)
-	{
-		cout << "We can't find your email adress. Please try again";
-		return;
-	}
+        if (flspec == 0 || flnum == 0 || fllett == 0)
+        {
+            cout << "Your password is uncorrect. Please try again\n";
+            continue;
+        }
 
 
-	cout << "Enter your message: ";
-	cin.get();
-	cin.getline(Comment, StartSize);
+        size = strlen(Login);
 
-	ofstream fout;
+        if (size < 8)
+        {
+            cout << "Your Login size is too low. Please try again\n";
+            continue;
+        }
 
-	fout.open("UserData.txt");
+        flspec = 0;
+        flnum = 0;
 
-	fout << "Login: " << Login << " Password: " << Pass << endl << "Text:" << endl << Comment;
+        if (strchr(Login, '@') != nullptr)
+            flspec = 1;
 
-	fout.close();
+        if (strchr(Login, '.') != nullptr)
+            flnum = 1;
 
-	fout.open("Data.txt");
+        if (flnum == 0 || flspec == 0)
+        {
+            cout << "We can't find your email adress. Please try again\n";
+            continue;
+        }
+        break;
+    }
+    while (true);
+
+    cout << "Enter your message: ";
+    cin.get();
+    cin.getline(Comment, StartSize);
+
+    ofstream fout;
+
+    fout.open("UserData.txt");
+
+    fout << "Login: " << Login << " Password: " << Pass << endl << "Text:" << endl << Comment;
+
+    fout.close();
+
+    fout.open("Data.txt");
 
 
-	fout << Comment;
+    fout << Comment;
 
-	fout.close();
+    fout.close();
 
 }
